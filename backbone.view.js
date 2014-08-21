cebb0f52d9b76e462867d2548ddb81cde6668c37
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-08-21 21:01:06 +0100

    initialise view with jquery

--- backbone.view.js
+++ backbone.view.js
@@ -7,3 +7,25 @@ el: 'body',
 initalize: function(){
 	this.html = 'Description:Wooden Toy House. ' +
 		'Price: $22. Quantity: 3.'
+		
+//Define new view.
+var InvoiceItemView2 = Backbone.View.extend({
+	// Set tag name and its attributes.
+	tagName: 'p',
+	className: 'item',
+	attributes: {
+	'align': 'left'
+	},
+	
+	//Intialise view object values.
+	initialize: function() {
+		this.html = 'Farm Animal Set. Price $17. Quantity: 1.'
+		},
+		
+		//Render view.
+		render: FUNCTION() {
+		
+		// Set HTML for the view element using jQUERY
+		$(this.el).html(this.html);
+		}
+		});
